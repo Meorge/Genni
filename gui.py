4c47c4744cda5fae7547bc5f6c744be26a76c60d
Malcolm Anderson <malcolminyo@gmail.com>
2021-10-30 22:57:00 -0700

    New UI partially hooked up, time elapsed label

--- gui.py
+++ gui.py
@@ -1,9 +1,12 @@
 from typing import List
-from PyQt5.QtCore import QSize, QThread, Qt, pyqtSignal, QPointF
+from PyQt5.QtCore import QSize, QThread, QTimer, Qt, pyqtSignal, QPointF
 from PyQt5.QtGui import QColor, QColorConstants, QFont, QPen
 from PyQt5.QtWidgets import QFrame, QGridLayout, QLabel, QMainWindow, QApplication, QProgressBar, QPushButton, QSizePolicy, QSplitter, QVBoxLayout, QWidget
 from PyQt5.QtChart import QChart, QChartView, QLineSeries
 import sys
+from datetime import datetime, timedelta
+from time import gmtime
+from os import environ
 
 from aitextgen.aitextgen.TokenDataset import TokenDataset
 from aitextgen.aitextgen.tokenizers import train_tokenizer
@@ -14,6 +17,12 @@ COLOR_PURPLE = QColor(180, 170, 255)
 COLOR_BLUE = QColor(170, 240, 255)
 COLOR_GREEN = QColor(220, 255, 170)
 COLOR_YELLOW = QColor(255, 250, 170)
+
+GENERATE_EVERY = 100
+SAVE_EVERY = 500
+TOTAL_STEPS = 5000
+
+SMOOTHING_FACTOR = 0.005
 
 class MyWindow(QMainWindow):
     def __init__(self):
@@ -34,17 +43,20 @@ class MyWindow(QMainWindow):
         self.chart.createDefaultAxes()
 
         self.chart.axisX().setRange(0,0)
-        self.chart.axisX().setTitleText("Step Number")
+        self.chart.axisX().setTitleText("Step")
         self.chart.axisX().setLabelFormat("%d")
         self.chart.axisY().setRange(0,0)
         
 
-        self.trainingLabel = QLabel("Training...  (7.5%)")
+        self.trainingLabel = QLabel("Training...")
         self.trainingLabelFont = QFont()
         self.trainingLabelFont.setBold(True)
         self.trainingLabelFont.setPointSizeF(self.trainingLabelFont.pointSizeF() * 2.0)
         self.trainingLabel.setFont(self.trainingLabelFont)
-        self.trainingLabel.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+        self.trainingLabel.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Maximum)
+
+        self.trainingSourceLabel = QLabel("Finetuning on the dataset \"training_data.txt\".")
+        self.trainingSourceLabel.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Maximum)
 
         self.chartView = QChartView(self.chart)
         self.gobutton = QPushButton("Start Training", clicked=self.doTraining)
@@ -54,25 +66,23 @@ class MyWindow(QMainWindow):
         self.splitter = QSplitter(self)
         self.splitter.addWidget(self.trainingInfo)
         self.splitter.addWidget(self.chartView)
+        self.splitter.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
 
         self.ly = QVBoxLayout()
-        self.ly.addWidget(self.trainingLabel, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.ly.addWidget(self.trainingLabel, alignment=Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+        self.ly.addWidget(self.trainingSourceLabel, alignment=Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
         self.ly.addWidget(self.splitter)
         self.ly.addWidget(self.gobutton)
 
-        # self.ly.addWidget(self.trainingLabel, 0, 0, 1, 2, Qt.AlignmentFlag.AlignCenter)
-        # self.ly.addWidget(self.chartView, 1, 1)
-        # self.ly.addWidget(self.trainingInfo, 1, 0, Qt.AlignmentFlag.AlignTop)
-        # self.ly.addWidget(self.gobutton, 2, 0, 1, 2)
-
         self.setCentralWidget(QWidget())
         self.centralWidget().setLayout(self.ly)
-
+        
     def doTraining(self):
         self.trainThread = ATGTrainer(self)
         self.trainThread.trainingStarted.connect(self.onTrainingStarted)
         self.trainThread.trainingEnded.connect(self.onTrainingEnded)
         self.trainThread.batchEnded.connect(self.onBatchEnded)
+        self.trainThread.timePassed.connect(self.trainingInfo.onTimePassed)
         self.trainThread.start()
 
     def onTrainingStarted(self):
@@ -82,9 +92,7 @@ class MyWindow(QMainWindow):
         self.gobutton.setEnabled(True)
 
     def onBatchEnded(self, steps, total, loss, avg_loss):
-        # self.stepLabel.setText(f'Step {steps}/{total} ({steps*100/total}%)')
-        # self.lossLabel.setText(f'Loss = {loss:.2f}, Avg = {avg_loss:.2f}')
-
+        self.trainingInfo.onBatchEnded(steps, total, avg_loss)
         self.chart.axisX().setRange(0, total)
         self.chart.axisY().setRange(0, max(loss, self.chart.axisY(self.lossSeries).max()))
 
@@ -95,17 +103,17 @@ class TrainingInformationView(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.currentStepLabel = LabeledValueView("Current Step", "150", COLOR_PURPLE)
-        self.totalStepLabel = LabeledValueView("Total Steps", "2000", COLOR_PURPLE)
+        self.currentStepLabel = LabeledValueView("Current Step", "---", COLOR_PURPLE)
+        self.totalStepLabel = LabeledValueView("Total Steps", f"{TOTAL_STEPS}", COLOR_PURPLE)
 
-        self.timeElapsedLabel = LabeledValueView("Elapsed", "00:13:24", COLOR_GREEN)
-        self.timeRemainingLabel = LabeledValueView("Remaining", "01:35:21", COLOR_GREEN)
+        self.timeElapsedLabel = LabeledValueView("Elapsed", "--:--:--", COLOR_GREEN)
+        self.timeRemainingLabel = LabeledValueView("Remaining", "--:--:--", COLOR_GREEN)
 
-        self.avgLossLabel = LabeledValueView("Avg.  Loss", "3.24", COLOR_YELLOW)
-        self.dAvgLossLabel = LabeledValueView("Change in Avg.  Loss", "-0.12", QColor(200, 255, 170))
+        self.avgLossLabel = LabeledValueView("Avg.  Loss", "-.--", COLOR_YELLOW)
+        self.dAvgLossLabel = LabeledValueView("Change in Avg.  Loss", "-.--", QColor(200, 255, 170))
 
-        self.stepsToGenTextLabel = LabeledValueView("Next Samples In", "200", COLOR_BLUE)
-        self.stepsToSaveModelLabel = LabeledValueView("Next Save In", "300", COLOR_BLUE)
+        self.stepsToGenTextLabel = LabeledValueView("Next Samples", "---", COLOR_BLUE)
+        self.stepsToSaveModelLabel = LabeledValueView("Next Save", "---", COLOR_BLUE)
 
         self.ly = QGridLayout(self)
 
@@ -116,7 +124,9 @@ class TrainingInformationView(QWidget):
         self.addDivider()
         self.addRow(self.timeElapsedLabel, self.timeRemainingLabel)
         self.addDivider()
-        self.addRow(self.avgLossLabel, self.dAvgLossLabel)
+        self.addRow(self.avgLossLabel, QWidget())
+        # self.ly.addWidget(QWidget(), self.currentGridRow, 0, 4, 2)
+        self.ly.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeading)
 
         self.setLayout(self.ly)
         self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Maximum)
@@ -133,6 +143,28 @@ class TrainingInformationView(QWidget):
         self.ly.addWidget(line, self.currentGridRow, 0, 1, 2)
         self.currentGridRow += 1
 
+    def onBatchEnded(self, steps, total, avg_loss):
+        self.currentStepLabel.setValue(str(steps))
+        self.totalStepLabel.setValue(str(total))
+
+        stepsToGen = (total - steps) % GENERATE_EVERY
+        stepsToSave = (total - steps) % SAVE_EVERY
+        self.stepsToGenTextLabel.setValue('Generating' if stepsToGen == 0 else str(stepsToGen))
+        self.stepsToSaveModelLabel.setValue('Saving' if stepsToSave == 0 else str(stepsToSave))
+
+        self.avgLossLabel.setValue(f'{avg_loss:.2f}')
+
+    def onTimePassed(self, passed: timedelta, remaining):
+        hours, rem = divmod(passed.seconds, 3600)
+        minutes, seconds = divmod(rem, 60)
+        passedStr = f'{hours:02d}:{minutes:02d}:{seconds:02d}'
+        self.timeElapsedLabel.setValue(passedStr)
+
+
+    def updateAvgSpeed(self, lastSpeed):
+        # https://stackoverflow.com/a/3841706
+        # TODO: calculate speeds to put into here!
+        self.averageSpeed = SMOOTHING_FACTOR * lastSpeed + (1 - SMOOTHING_FACTOR) * self.averageSpeed
 
 
 class LabeledValueView(QWidget):
@@ -161,8 +193,12 @@ class LabeledValueView(QWidget):
 
         self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Maximum)
 
+    def setValue(self, value: str):
+        self.valueLabel.setText(value)
+
     def setValueColor(self, color: QColor):
         self.valuePalette.setColor(self.valueLabel.foregroundRole(), color)
+
 
 class ATGTrainer(QThread):
     trainingStarted = pyqtSignal()
@@ -171,12 +207,32 @@ class ATGTrainer(QThread):
     sampleTextGenerated = pyqtSignal(list)
     modelSaved = pyqtSignal(int, int, str)
 
+    timePassed = pyqtSignal(timedelta, float)
+
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.timePassedTimer = QTimer()
+        self.timePassedTimer.setInterval(100)
+        self.timePassedTimer.timeout.connect(self.onTimePassed)
+
+        self.trainingStarted.connect(self.onTrainingStarted_main)
+        self.trainingEnded.connect(self.onTrainingEnded_main)
+
+    def onTrainingStarted_main(self):
+        self.startTime = datetime.now()
+        self.timePassedTimer.start()
+
+    def onTrainingEnded_main(self):
+        self.timePassedTimer.stop()
+
+    def onTimePassed(self):
+        currentTime = datetime.now()
+        elapsed = currentTime - self.startTime
+        self.timePassed.emit(elapsed, 0)
 
     def run(self):
         file_name = "training.txt"
-        train_tokenizer(file_name)
+        # train_tokenizer(file_name)
         tokenizer_file = "aitextgen.tokenizer.json"
         config = GPT2ConfigCPU()
 
@@ -191,7 +247,7 @@ class ATGTrainer(QThread):
             'on_model_saved': self.onModelSaved
         }
 
-        self.ai.train(self.data, batch_size=1, num_steps=1000, generate_every=100, save_every=100, print_generated=False, print_saved=False, callbacks=callbacks)
+        self.ai.train(self.data, batch_size=1, num_steps=TOTAL_STEPS, generate_every=GENERATE_EVERY, save_every=SAVE_EVERY, print_generated=False, print_saved=False, callbacks=callbacks)
 
     def onTrainingStarted(self):
         # print("Training has started!")
@@ -214,6 +270,8 @@ class ATGTrainer(QThread):
         self.modelSaved.emit(steps, total, dir)
 
 if __name__ == "__main__":
+    environ["TOKENIZERS_PARALLELISM"] = "false"
+    environ["OMP_NUM_THREADS"] = "1"
     app = QApplication([])
     window = MyWindow()
     window.show()
