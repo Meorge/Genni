70d1d7f0e64facb79f9a6a3e7815871ac29b458f
Malcolm Anderson <malcolminyo@gmail.com>
2021-11-27 21:48:02 -0800

    "Training Complete" has slide-in animation (#1)
    The "Done" button doesn't have an animation, but I don't think it's as important

--- Views/Training/TrainingInProgressView.py
+++ Views/Training/TrainingInProgressView.py
@@ -1,6 +1,6 @@
-from PyQt6.QtCore import QSize, Qt, QPointF, pyqtSignal
+from PyQt6.QtCore import Qt, QPointF
 from PyQt6.QtGui import QColor, QPen
-from PyQt6.QtWidgets import QHBoxLayout, QPushButton, QSizePolicy, QSplitter, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QHBoxLayout, QPushButton, QSplitter, QVBoxLayout, QWidget
 from PyQt6.QtCharts import QChart, QChartView, QLineSeries
 
 from Views.Training.TrainingInformationView import TrainingInformationView
@@ -10,10 +10,22 @@ from Views.SwipingPageView import SwipingPageView
 class TrainingInProgressView(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.hyperparams = {}
+
         # We need a title for each state - in-progress, and finished.
+        self.slidingTitleContainer = SwipingPageView(self)
+
         self.title = WizardTitleView(self)
         self.title.setTitle('Training...')
-        self.title.setSubtitle(f'''Finetuning... This might take a while.''')
+        self.title.setSubtitle(f'''This might take a while.''')
+
+        self.trainingDoneTitle = WizardTitleView(self)
+        self.trainingDoneTitle.setTitle('Training Complete')
+        self.trainingDoneTitle.setSubtitle(f'''Finetuning on \"{self.hyperparams.get('dataset', {}).get('meta', {}).get('title', 'an unknown dataset')}\" has finished. You can view the statistics on this training session at any time from the Repository view.''')
+
+        self.slidingTitleContainer.addWidget(self.title)
+        self.slidingTitleContainer.addWidget(self.trainingDoneTitle)
+
 
         self.lossSeries = QLineSeries()
         self.lossSeries.setName("Loss")
@@ -44,24 +56,24 @@ class TrainingInProgressView(QWidget):
         self.splitter.addWidget(self.chartView)
 
         # Control buttons
-        self.genSamplesButton = QPushButton('Generate Samples', self, enabled=False)
-        self.saveModelButton = QPushButton('Save Model', self, enabled=False)
+        self.genSamplesButton = QPushButton('Generate Samples', enabled=False)
+        self.saveModelButton = QPushButton('Save Model', enabled=False)
 
         # Done button (for when training is complete)
-        self.doneButton = QPushButton('Close', self)
+        self.doneButton = QPushButton('Close')
         self.doneButton.setVisible(False)
 
         self.buttonLy = QHBoxLayout()
-        self.buttonLy.addWidget(self.genSamplesButton)
-        self.buttonLy.addWidget(self.saveModelButton)
+        # self.buttonLy.addWidget(self.genSamplesButton)
+        # self.buttonLy.addWidget(self.saveModelButton)
         self.buttonLy.addWidget(self.doneButton)
 
         self.ly = QVBoxLayout(self)
-        self.ly.addWidget(self.title, alignment=Qt.AlignmentFlag.AlignHCenter)
+        self.ly.addWidget(self.slidingTitleContainer, alignment=Qt.AlignmentFlag.AlignHCenter)
         self.ly.addWidget(self.splitter)
-        # self.ly.addLayout(self.buttonLy)
+        self.ly.addLayout(self.buttonLy)
 
-        self.hyperparams = {}
+        
 
     def setHyperparameters(self, hyperparams: dict):
         self.hyperparams = hyperparams
@@ -71,11 +83,10 @@ class TrainingInProgressView(QWidget):
         print('Training has started')
 
     def onTrainingEnded(self):
-        self.title.setTitle('Training Complete')
-        self.title.setSubtitle(f'''Finetuning on \"{self.hyperparams.get('dataset', {}).get('meta', {}).get('title', 'an unknown dataset')}\" has finished. You can view the statistics on this training session at any time from the Repository view.''')
+        self.trainingDoneTitle.setSubtitle(f'''Finetuning on \"{self.hyperparams.get('dataset', {}).get('meta', {}).get('title', 'an unknown dataset')}\" has finished. You can view the statistics on this training session at any time from the Repository view.''')
+        self.slidingTitleContainer.slideInWgt(self.trainingDoneTitle)
         self.genSamplesButton.setVisible(False)
         self.saveModelButton.setVisible(False)
-        self.stopTrainingButton.setVisible(False)
         self.doneButton.setVisible(True)
 
     def onStopTriggered(self):
